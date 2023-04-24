2fab3150bde25eec91e931c0d7d9c006e835f717
Eito Takase <eito01178@gmail.com>
2023-04-24 11:28:50 +0900

    hc更新

--- backend/routes/api/hc/index.ts
+++ backend/routes/api/hc/index.ts
@@ -1,9 +1,23 @@
 import { HandlerContext } from "$fresh/server.ts";
+import { supabase } from "../../../core/db/supabase.ts";
 
-export const handler = (_req: Request, _ctx: HandlerContext) => {
+export const handler = async (_req: Request, _ctx: HandlerContext) => {
+  const { error: deleteError } = await supabase
+    .from("hc")
+    .delete()
+    .gt("id", -1);
+  const { error: insertError } = await supabase.from("hc").insert({ id: 0 });
+  const { error: selectError } = await supabase.from("hc").select("*").limit(1);
+
   return new Response(
     JSON.stringify({
-      ping: "pong",
+      db: {
+        delete: deleteError ? "error" : "ok",
+        insert: insertError ? "error" : "ok",
+        select: selectError ? "error" : "ok",
+      },
+      message: "success",
+      status: 200,
     })
   );
 };
