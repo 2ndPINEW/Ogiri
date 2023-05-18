27b98d3467d94b4a419af741ac60f3be96a22296
Eito Takase <eito01178@gmail.com>
2023-05-19 01:49:58 +0900

    もっと長くていいんじゃないかい

--- backend/routes/api/groups/[groupId]/ogiris/[ogiriId]/report.ts
+++ backend/routes/api/groups/[groupId]/ogiris/[ogiriId]/report.ts
@@ -72,7 +72,7 @@ export const handler = async (req: Request, ctx: HandlerContext) => {
 
   const headers = {
     "Cache-Control": isComplete
-      ? "public, max-age=0, s-maxage=3600"
+      ? "public, max-age=0, s-maxage=86400"
       : "public, max-age=0, s-maxage=10",
   };
 
