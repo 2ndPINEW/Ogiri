bd96aaabd7200aeef14c04f9bad985a9585c4da3
Eito Takase <eito01178@gmail.com>
2023-05-19 02:51:12 +0900

    proxy入らなかった

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'chot-ogiri yahhoo';
+  title = 'chot-ogiri';
+  isProduction = environment.production;
 }
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8040/api/',
+  siteTitle: 'ChotOgiri',
+};
