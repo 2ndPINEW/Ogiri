f438b80a5a0b897678560d167bfcbff1cc9731e2
Eito Takase <eito01178@gmail.com>
2023-05-22 00:36:55 +0900

    prompt調整

--- backend/core/ogiri/scoring.ts
+++ backend/core/ogiri/scoring.ts
@@ -8,20 +8,21 @@ export const scoring = async (
   reason: string;
 }> => {
   const prompt = {
-    prompt: `以下の大喜利を採点してください、採点理由はなるべく褒めて伸ばすようにしてください
+    prompt: `以下の大喜利を採点してください、得点少し厳し目に、採点理由はなるべく褒めて伸ばすようにしてください
     [お題]
     ${odai}
     
     [回答]
     ${answer}`,
-    exampleDescription: "こんなスタバは嫌だというお題に対しての解答例",
+    exampleDescription:
+      "こんなスタバは嫌だというお題に対して「家系ラーメンが出てくる」と回答があった時の採点",
     response: {
       score: {
         example: "70",
-        description: "0 ~ 100",
+        description: "0 ~ 100の特典",
       },
       reason: {
-        example: "家系ラーメンが出てくる",
+        example: "独創的なアイデアな上に意外性もあってよかったです。",
         description: "採点理由",
       },
     },
