b5467ea5fff5442f74300fe1393f4974f6b729ae
Eito Takase <eito01178@gmail.com>
2023-05-21 16:36:00 +0900

    interval

--- frontend/src/app/routed/index/index.component.ts
+++ frontend/src/app/routed/index/index.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription, timer } from 'rxjs';
 import { isApiError } from 'src/app/shared/services/api.interface';
 import { OgiriService } from 'src/app/shared/services/ogiri.service';
 
@@ -12,26 +13,36 @@ export class IndexComponent {
   showUi = false;
   error: false | string = false;
 
+  subscription = new Subscription();
+
   constructor(private ogiriService: OgiriService, private router: Router) {}
 
   ngOnInit(): void {
-    this.ogiriService.openOgiris$().subscribe({
-      next: (res) => {
-        if (res.length === 1) {
-          this.router.navigate(['play', res[0].id]);
-        } else if (res.length > 1) {
-          this.router.navigate(['play']);
-        } else {
-          this.showUi = true;
-        }
-      },
-      error: (err) => {
-        this.showUi = true;
-        this.error = '通信エラーが発生しました';
-        if (isApiError(err)) {
-          this.error = `通信エラーが発生しました: ${err.message}`;
-        }
-      },
-    });
+    this.subscription.add(
+      timer(0, 10000).subscribe(() => {
+        this.ogiriService.openOgiris$().subscribe({
+          next: (res) => {
+            if (res.length === 1) {
+              this.router.navigate(['play', res[0].id]);
+            } else if (res.length > 1) {
+              this.router.navigate(['play']);
+            } else {
+              this.showUi = true;
+            }
+          },
+          error: (err) => {
+            this.showUi = true;
+            this.error = '通信エラーが発生しました';
+            if (isApiError(err)) {
+              this.error = `通信エラーが発生しました: ${err.message}`;
+            }
+          },
+        });
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
